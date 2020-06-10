c9d666a656f441882081d72332818ab1f6e2e0ec
Igor Olkhovskiy <i.olkhovskiy@yandex.ru>
2020-06-10 22:53:32 +0300

    voc indexer for segmentation has been added

---
+++ voc_dataset/utils.py
@@ -0,0 +1,46 @@
+from os.path import isfile
+
+
+def load_label_map(path_to_config):
+    if not isfile(path_to_config):
+        raise FileNotFoundError("Invalid path to label map file")
+    out = {}
+    with open(path_to_config, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            words = line.strip().split()
+            if len(words) != 5:
+                error_msg = "Label map has invalid structure. " + \
+                    "Target: id:int, label:string, b:int, g:int, r:int" + \
+                    " split symbol is space. + Got "+str(len(words))+" words"
+                raise RuntimeError(error_msg)
+            idx, label = int(words[0]), words[1]
+            b, g, r = int(words[2]), int(words[3]), int(words[4])
+            out[label] = {"id": idx, "color": (b, g, r)}
+    return out
+
+
+def make_id2label(label_map):
+    out = {}
+    for label in label_map.keys():
+        out[label_map[label]["id"]] = str(label)
+    return out
+
+
+def make_color2label(label_map):
+    out = {}
+    for label in label_map.keys():
+        out[label_map[label]["color"]] = str(label)
+    return out
+
+
+def color_histogram(image_bgr):
+    hist = {}
+    for j in range(image_bgr.shape[0]):
+        for i in range(image_bgr.shape[1]):
+            pixel = tuple(image_bgr[j, i, :])
+            if pixel not in hist.keys():
+                hist[pixel] = 0
+            hist[pixel] += 1
+    return hist
+
---
+++ voc_dataset/voc_index.py
@@ -0,0 +1,69 @@
+from glob import glob
+from os.path import join, isdir
+import cv2
+import datetime
+
+from voc_dataset.utils import load_label_map, make_color2label, color_histogram
+
+
+class VocIndex:
+
+    def __init__(self, root, label_map_path):
+        self.label_map = load_label_map(label_map_path)
+        self.color2label = make_color2label(self.label_map)
+        self.root = root
+        self.index = {}
+        self.linear_index = []
+        self.idx = 0
+        if not isdir(self.root):
+            raise ValueError("Invalid dataset path")
+        self.all_images_list = glob(join(self.root, "JPEGImages/*.jpg"))
+        self.all_segmentation_maps_list = glob(join(self.root, "SegmentationClass/*.png"))
+        with open(join(self.root, "ImageSets/Segmentation/train.txt"), "r") as f:
+            lines = f.readlines()
+            segmented_images = [x.strip() for x in lines]
+        for sample_id, sample_name in enumerate(segmented_images):
+            sample_img_path = self.root + "/JPEGImages/" + sample_name + ".jpg"
+            sample_map_path = self.root + "/SegmentationClass/" + sample_name + ".png"
+            sample_map = cv2.imread(sample_map_path)
+            hist = color_histogram(sample_map)
+            objects = []
+            for hist_val in hist.keys():
+                if hist_val in self.color2label.keys():
+                    label = self.color2label[hist_val]
+                    if label not in self.index.keys():
+                        self.index[label] = []
+                    objects.append(label)
+                    self.index[label].append((sample_img_path, sample_map_path))
+            self.linear_index.append((sample_img_path, sample_map_path, objects))
+            for i in range(10):
+                if sample_id == int(len(segmented_images) * i * 0.1):
+                    print(str(i*10), "% loaded",  datetime.datetime.now())
+        return
+
+    def __getitem__(self, item):
+        return self.linear_index[item]
+
+    def __str__(self):
+        return "Pascal VOC for segmentation"
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.idx < len(self.linear_index):
+            out = self.linear_index[self.idx]
+            self.idx += 1
+            return out
+        else:
+            raise StopIteration
+
+    def __len__(self):
+        return len(self.linear_index)
+
+
+if __name__ == "__main__":
+    index = VocIndex("/home/igor/datasets/VOC_2012/trainval",
+                     "/home/igor/github/my/UNet_segmentator/configs/label_map.txt")
+    for sample in index:
+        print(sample)
